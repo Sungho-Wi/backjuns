6f5370bb9adafe56c317c94d13ca6b4d31c78a80
Sungho-Wi <fornals222@gmail.com>
2021-03-31 18:00:40 +0900

    Create 1916_최소비용구하기.py

---
+++ Python/1916_최소비용구하기.py
@@ -0,0 +1,39 @@
+import heapq
+import sys
+
+input = sys.stdin.readline
+INF = int(1e9)
+
+정점개수 = int(input())
+간선개수 = int(input())
+
+graph = [[] for _ in range(정점개수 + 1)]
+distance = [INF] * (정점개수 + 1)
+
+for _ in range(간선개수):
+    u, v, w = map(int, input().split())
+    graph[u].append((v, w))
+
+
+start, end = map(int, input().split())
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+
+    while q:
+        dist, now = heapq.heappop(q)
+
+        if distance[now] < dist:
+            continue
+
+        for d in graph[now]:
+            cost = distance[now] + d[1]
+            if cost < distance[d[0]]:
+                distance[d[0]] = cost
+                heapq.heappush(q, (cost, d[0]))
+
+dijkstra(start)
+
+print(distance[end])
